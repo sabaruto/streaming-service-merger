3eba345914bd206d1d10787feefd174832d1b2dc
Theodosia <theodoreaaronobelley@gmail.com>
2025-01-21 22:03:54 +0000

    add frontend routing and initalise go

--- backend/authorisation/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from authorisation import views
-
-urlpatterns = [
-    path("", views.AuthoriseView.as_view(), name="user"),
-    path("csrf/", views.CSRFView.as_view(), name="csrf"),
-    path("register/", views.RegisterView.as_view(), name="register"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
-]
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,38 +1,7 @@
 export default function Home() {
-  return (
-    <article className="form-container">
-      <header>
-        <h2>
-          Music Manager
-        </h2>
-      </header>
-      <section>
-        <h3>
-          Register
-        </h3>
-        <form>
-          <div>
-            <label>Username</label>
-            <input/>
-          </div>
-          <div>
-            <label>Password</label>
-            <input/>
-          </div>
-          <div>
-            <label>Verify Password</label>
-            <input/>
-          </div>
-        </form>
-        <footer>
-          <button>
-            <h5>Register</h5>
-          </button>
-        </footer>
-      </section>
-      <footer>
-        <small>Already have an account? <a>log in</a></small>
-      </footer>
-    </article>
-  );
+    return (
+        <article className="">
+            hello
+        </article>
+    );
 }
