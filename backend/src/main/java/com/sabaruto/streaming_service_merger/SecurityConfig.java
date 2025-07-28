62ee2976bae365bd4e96ec8d6424e351757f4b69
sabaruto <theodoreaaronobelley@hotmail.co.uk>
2025-07-28 23:32:25 +0100

    initial commit

--- backend/src/main/java/com/sabaruto/streaming_service_merger/SecurityConfig.java
+++ backend/src/main/java/com/sabaruto/streaming_service_merger/SecurityConfig.java
@@ -1,4 +1,58 @@
 package com.sabaruto.streaming_service_merger;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(
+            (requests) -> requests
+                .requestMatchers("/login", "/register", "/logout").permitAll()
+                .anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults())
+            .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+                                                       PasswordEncoder passwordEncoder) {
+
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("spring")
+            .password(encoder.encode("secret"))
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return encoder;
+    }
 }
--- backend/src/main/java/com/sabaruto/streaming_service_merger/StreamingServiceMergerApplication.java
+++ backend/src/main/java/com/sabaruto/streaming_service_merger/StreamingServiceMergerApplication.java
@@ -3,11 +3,15 @@ package com.sabaruto.streaming_service_merger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class StreamingServiceMergerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamingServiceMergerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        log.info("Logging");
+        SpringApplication.run(StreamingServiceMergerApplication.class, args);
+    }
 
 }
